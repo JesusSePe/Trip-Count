1ab9682197a878f6e80eff79eeb0c400e9e6d4cb
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-12-10 23:04:00 +0100

    footer and header finished

---
+++ static/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style><?php include_once(dirname(__DIR__).'/styles/header.css'); ?></style>
+    <link rel="icon" type="icon/png" href="../img/coin.png"/>
+    <title>TRIP-COUNT</title>
+</head>
+<body>
+<div class="header">
+  <div class="tripcount">
+    <a class="logo">TRIP-COUNT</a>
+  </div>
+  <div class="header-right">
+    <a class="button" href="home.php"><span>Home</span></a>
+  </div>
+</div>
+</body>
+</html>
+
