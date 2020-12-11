a3a4c178fdf197bdbd5ab697540cf31a2074a15e
dchcobra <davidcasthen@gmail.com>
2020-12-11 15:52:39 +0100

    quitado header y body

--- static/header.php
+++ static/header.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style><?php include_once(dirname(__DIR__).'/styles/header.css'); ?></style>
-    <title>TRIP-COUNT</title>
-</head>
-<body>
 <div class="header">
   <div class="tripcount">
     <a class="logo">TRIP-COUNT</a>
@@ -15,6 +6,3 @@
     <a class="button" href="home.php"><span>Home</span></a>
   </div>
 </div>
-</body>
-</html>
-
