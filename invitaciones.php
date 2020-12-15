f99478b00092d6406f88252142eca1bda1587bc8
dchcobra <davidcasthen@gmail.com>
2020-12-15 20:54:25 +0100

    Input añadidos

--- invitaciones.php
+++ invitaciones.php
@@ -1,45 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/main.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <script src="static/js/functions.js"></script>
     <title>Invitaciones</title>
 </head>
 <body>
 <?php include_once(dirname(__DIR__) . "/Trip-Count/static/header.php");?>
 <?php $viaje = 'example'?>
-<div class="main-content">
+<div class="main-content forminv">
     <h1><?php echo $viaje?></h1>
+    <p>Introduce los correos para invitar a tus amigos:</p>
+    <div">
 
- </head>
- <body>
-   <div id="form" class="form">
-    <form action="">
-        <input type="email">
+        <div id="form" class="forminv">
+            <form action="invitaciones.php" method="get">
+                <input name="emails[]" type="email" placeholder="example@gmail.com">
+            </div>
+            <input class="button" type="submit">
+        </form>
+        <button onclick="crearInputInv()" id="emailsInv" class="button"><span>Añadir</span></button>
     </div>
-    <input type="submit">
-</form>
-<button onclick="crearInputInv()" id="btn_agregar">+</button>
 </div>
+<?
+      print $_GET["emails"];
+?>
 <?php include_once(dirname(__DIR__) . "/Trip-Count/static/footer.php");?>
 
 </body>
 </html>
-
-<script>
-      function crearInputInv(){
-         
-         var div = document.getElementById("form");
-         var input = document.createElement("INPUT");         
-         input.type = 'email';
-
-         div.appendChild(input);
-      } 
-      window.onload = function(){
-         
-         var btnAdd = document.getEmentById("btn_agregar");   
-         btnAdd.onclick = crearDin;
-      }      
-   </script>
