67e63be923bb5b3d9a191fe9f85e7593bef9d63e
Pradas137 <pradas137@gmail.com>
2020-12-16 18:11:15 +0100

    login y home

--- login.php
+++ login.php
@@ -11,30 +11,6 @@
   </head>
     <body>
     <?php include_once(dirname(__DIR__) . "/Trip-Count/static/header.php");?>
-    <?php
-      if ($_SERVER["REQUEST_METHOD"] == "POST" and isset($_POST["username"]) and isset($_POST["userPass"])) {
-        $hostname = "localhost";
-        $dbname = "tripcount";
-        $username = "root";
-        $pw = "";
-        $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $pw);
-
-        $_username = $_POST["username"];
-        $_password = $_POST["userPass"];
-          
-        $query = $pdo->prepare("SELECT * FROM users WHERE uname = ? AND pwd = ?");
-        $query->bindParam(1, $_username);
-        $query->bindParam(2, $_password);
-        $query->execute();
-        $row = $query -> fetch();
-        if ($row != false) {
-          systemMSG('success', 'Usuario correcto'); //NO LO MUESTRA PORQUE SE PASA DIRECTAMENTE
-          Redirect('home.php');
-        } else {
-          systemMSG('error', 'Usuario incorrecto');
-        }
-      }
-     ?>
   <body>
     <?php include_once(dirname(__DIR__) . "/Trip-Count/static/header.php");?>
     <div class="menu main-content">
@@ -42,17 +18,63 @@
         <div></div>
         <div class="logo">LOGIN</div>
         <div class="loginitem">
+          <?php  
+          session_start();  
+          $host = "localhost";  
+          $username = "adrian";  
+          $password = "Hakantor";  
+          $database = "tripcount";  
+          $message = "";  
+          try{
+            $connect = new PDO("mysql:host=$host; dbname=$database", $username, $password);
+            $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+            if(isset($_POST["submit"])){
+              if(empty($_POST["mail"]) || empty($_POST["pwd"])){
+                $message = systemMSG('info', 'All fields are required');}
+                else{
+                  $usernameEmail = $_POST["mail"];
+                  $pwd = $_POST["pwd"];
+                  /*$hash_password= hash('sha256', $pwd); //Password encryption */
+                  $query = "SELECT * FROM users WHERE mail = :mail AND pwd = :pwd";
+                  $statement = $connect->prepare($query);
+                  $statement->bindParam("mail", $usernameEmail) ;
+                  /*$statement->bindParam("hash_password", $hash_password) ;*/
+                  $statement->bindParam("pwd", $pwd) ;
+                  $statement->execute(
+                    array(
+                      'mail'     =>     $_POST["mail"],
+                      'pwd'     =>     $_POST["pwd"]
+                    )
+                  );
+                  /*$row = $statement->fetch();*/
+                  $count = $statement->rowCount();
+                  if($count > 0)
+                  {
+                    $_SESSION["mail"] = $_POST["mail"];
+                    header("location:home.php");
+                  }
+                  else
+                  {
+                    $message = systemMSG('error', 'Wrong Data');
+                  }
+                }
+              }
+            }
+            catch(PDOException $error){
+              $message = $error->getMessage();
+            }
+            ?>
           <form action="" method="post" class="form formlogin">
             <div class="formfield">
               <label class="user" for="loginemail"><span class="hidden"> Email</span></label>
-              <input id="loginemail" type="text" class="forminput" name="username" placeholder="Email" required>
+              <input id="loginemail" type="text" class="forminput" name="mail" placeholder="Email" required>
             </div>
             <div class="formfield">
               <label class="lock" for="loginpassword"><span class="hidden"> Password</span></label>
-              <input id="loginpassword" name="userPass" type="password" class="forminput" placeholder="Password" required>
+              <input id="loginpassword" name="pwd" type="password" class="forminput" placeholder="Password" required>
             </div>
             <div class="formfield">
-              <input type="submit" value="Login" class="button">
+              <input type="submit" name="submit" value="Login" class="button">
               <span></span>
             </div>
           </form>
