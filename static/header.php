26630be6511a3dc2571da3e88f0dc21ddf43fc7a
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2021-01-20 21:21:45 +0100

    solucionado que no muestre boton de home en login y register

--- static/header.php
+++ static/header.php
@@ -1,16 +1,32 @@
 <header>
-  <div class="header">
+<?php 
+if (($_SERVER['REQUEST_URI'] === '/Trip-Count/login.php') || ($_SERVER['REQUEST_URI'] === '/Trip-Count/register.php')) {
+    echo '<div class="header">
     <div class="tripcount">
       <a class="logo">TRIP-COUNT</a>
     </div>
-    <div>
-    <?php if(isset($_SESSION)){  
+    <div>';
+    if(isset($_SESSION)){  
       echo '<a class="logo"><span class="user">' . $_SESSION["name"] . '</span></a>';    
     }
-    ?>
+    echo '
+    </div>
+  </div>';
+} else {
+  echo '<div class="header">
+    <div class="tripcount">
+      <a class="logo">TRIP-COUNT</a>
+    </div>
+    <div>';
+    if(isset($_SESSION)){  
+      echo '<a class="logo"><span class="user">' . $_SESSION["name"] . '</span></a>';    
+    }
+    echo '
     </div>
     <div class="header-right">
       <a class="button" href="home.php" accesskey="h"><span><u>H</u>ome</span></a>
     </div>
-  </div>
+  </div>';
+}
+?>
 </header>
