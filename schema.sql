aa5925d42f75b041771373bdc12e2aff9a4c8765
Pradas137 <55705052+Pradas137@users.noreply.github.com>
2021-01-20 21:47:38 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,6 +8,13 @@ CREATE TABLE users (
 	PRIMARY KEY(id_user)
 );
 
+CREATE TABLE IF NOT EXISTS `images_tabla` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imagenes` longblob NOT NULL,
+  `creado` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ; 
+
 CREATE TABLE friends (
 	id_user MEDIUMINT NOT NULL,
 	id_friend MEDIUMINT NOT NULL,
